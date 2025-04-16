e1bfe348304bac9d227d75b4697f2146ae56f15a
mascencerro <mascencerro@gmail.com>
2025-04-16 18:58:12 +0000

    add alt_xsession.sh to client procedure

--- server_fs/srv/client_fs/etc/profile.d/30-startx.sh
+++ server_fs/srv/client_fs/etc/profile.d/30-startx.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+SERVER="$(cat /etc/next_server)"
+
+wget "http://${SERVER}/alpine/client/alt_xsession.sh" -O /tmp/alt_xsession.sh
+
 exec startx
---
+++ server_fs/srv/www/alpine/client/alt_xsession.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# xterm -geometry 270x40+20-20
+
+python3 /usr/local/bin/login.py
+
