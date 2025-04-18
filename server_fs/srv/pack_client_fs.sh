f273b52a72135015dd5801c3d9ab62169c1d0787
mascencerro <mascencerro@gmail.com>
2025-04-18 18:08:48 +0000

    move repack to pack_client_fs.sh

--- server_fs/srv/pack_client_fs.sh
+++ server_fs/srv/pack_client_fs.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
+# Need GNU tar for this (busybox tar doesn't provide -r)
+apk add tar
+
 tar -czvf "/srv/www/alpine/build/client_fs.tar.gz" -C "/srv/client_fs" .
+
+# Repackage changes made to client_fs files during server install
+# Uncompress APK overlay tarball
+gunzip /srv/www/alpine/client/thinclient.apkovl.tar.gz
+
+# etc/next_server
+tar -vf /srv/www/alpine/client/thinclient.apkovl.tar -C /srv/client_fs/ -r etc
+
+# etc/apk/repositories
+tar -vf /srv/www/alpine/client/thinclient.apkovl.tar -C /srv/client_fs/ -r usr
+
+# Compress APK overlay tarball
+gzip /srv/www/alpine/client/thinclient.apkovl.tar
+
+
+
+
+
--- setup.sh
+++ setup.sh
@@ -91,24 +91,8 @@ set_client_next_server() {
 pack_client_fs() {
     echo "Making some changes to client filesystem and repacking"
 
-    # Need GNU tar for this (busybox tar doesn't provide -r)
-    apk add tar
-
     # Pack up client_fs tree for custom APK overlay build usage
     sh /srv/pack_client_fs.sh
-
-    # Repackage changes made to client_fs files during server install
-    # Uncompress APK overlay tarball
-    gunzip /srv/www/alpine/client/thinclient.apkovl.tar.gz
-
-    # etc/next_server
-    tar -vf /srv/www/alpine/client/thinclient.apkovl.tar -C /srv/client_fs/ -r 'etc/next_server'
-
-    # etc/apk/repositories
-    tar -vf /srv/www/alpine/client/thinclient.apkovl.tar -C /srv/client_fs/ -r 'etc/apk/repositories'
-
-    # Compress APK overlay tarball
-    gzip /srv/www/alpine/client/thinclient.apkovl.tar
 
 }
 
